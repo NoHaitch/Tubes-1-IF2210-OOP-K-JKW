90c2d4a07c7f1ead1a6eb3d29851b5a4065c730d
Kharris-Khisunica <kharriskhisunica07@gmail.com>
2024-04-11 23:47:13 +0700

    shop.hpp updated

--- src/header/shop.hpp
+++ src/header/shop.hpp
@@ -12,24 +12,21 @@
 
 using namespace std;
 
-// #include "Yang ngurus inventori"
-// #include "Player"
-
-//
+/**
+ * @brief Class Shop used by All Player
+ * 
+ * \note - Childern: BlackMarket
+ * 
+ */
 class Shop{
 
 protected:
 
-    
-
-    
-    /* Configuration Data 
-    vector<Animal> animals;         // All Animals info from config
-    vector<Plant> plants;           // 
-    vector<Product> products;
-    vector<Building> buildings;
-    */
-    
+    int item_number;         // Item's item_number number / Item number
+    string code;    // Item's Code
+    int Price;      // Item's Price
+    bool isQtyInf;  // Bool Checker for Qty
+    float Qty;      // Qty Float to facilitate INFINITY
     
 
 public:
@@ -37,16 +34,16 @@ public:
     /**
      * @brief Shop ctor
      * 
-     * 
+     * \note if item came from Animal or Plant then isQtyInf = True, Qty = INFINITY else isQtyInf = False, Qty = 0
     */
     Shop();
-    //Ada inventori shop. 
-    //Qty Tanaman dan Hewan = infinite
-    //Baca config tanaman -> ambil ALL kecuali ID -> simpen di vektor
-    //Baca config hewan -> ambil ALL Kecuali ID -> simpen di vector
-    //Qty bangunan and Product = 0
-    //Baca config product -> ambil ALL kecuali ID, Qty = 0 -> simpen di vector
-    //Baca config building -> ambil ALL kecuali ID, Qty = 0 -> simpen di vector
+    // [[item_number, code, Price, isQtyInf, Qty]]
+
+    /**
+     * @brief 
+     * 
+     */
+    Shop(int _id, string _code, int _Price, bool isQtyInf, float _Qty);
     
 
     /**
@@ -56,53 +53,45 @@ public:
     */
     ~Shop();
 
+
+    //**************** GETTER  *****************//
+
+    /**
+     * @brief Get the Code with item_number = item_number
+     * 
+     * @param int item_number 
+     * @return string Code
+     */
+    string getCode (int item_number);
+
+
+    /**
+     * @brief Get Price of the item with Code = CODE from Shop
+     * 
+     * @param string _code
+     * 
+     */
+    int getPrice(string code);
+
+    /**
+     * @brief Get the Name of the item with Code = code from Shop
+     * 
+     * @param code 
+     * @return string 
+     */
+    string getName(string code);
+
+    /**
+     * @brief Update the Qty of the items with code = code
+     * 
+     * @param string code 
+     * @param int change 
+     * @param bool isAdd. if True than Qty += change otherwise Qty -= change
+     */
+    void updateQty(string code, int change, bool isAdd);
     
 
-    
-    //**************** Quantity Modifier *****************//
-    /**
-     * @brief Modified the Qty of Item with ID = id 
-     * 
-     * @param int _id
-     * @param int change        
-     * @param Shop& shop        
-     * @param bool isAdd 
-     */
-
-    void item_Qty_Modifier(int _id, int change, Shop& shop, bool isAdd);
-
-    
-
-    /**
-     * @brief Get Data from a number selected by user 
-     * 
-     * @param int _id
-     */
-
-    any get_Data(int _id);
-
-    /**
-     * @brief Get Price of the item with ID = id
-     * 
-     * @param int _id
-     * 
-     */
-
-    int get_Price(int _id);
-
-    /**
-     * @brief Get id from item code
-     * 
-     */
-    int get_ID_from_code(string code);
-
-    /**
-     * @brief 
-     * 
-     */
-
-
-    //*************** Show Inventory *******************//
+    //*************** Show Shop Inventory *******************//
 
     /**
      * @brief Show Shop Inventory Based on Role
@@ -113,65 +102,93 @@ public:
     void Show_Shop_Inventory(string Role) const;
 
 
-    /*************** GET Vector *************************/
 
-     /**
-     * @brief Get Animal Config from Game.hpp
-     * 
-     */
-    void AddAnimalConfig(string _code, string _name, string _foodtype, int _weighttoHarvest, int _price, float _Qty);
+    //**************** Validator *****************//
 
     /**
-     * @brief Get Plant Config from Game.hpp
+     * @brief Check if player has enough money and empty inv slot
      * 
+     * @param item_number 
+     * @param Quantity 
+     * 
+     * @return true if enough
+     * @return false otherwise
      */
-    void AddPlantConfig(string _code, string _name, string _foodtype, int _durationtoHarvest, int _price, float _Qty);
+    bool isBuyValid (int item_number, int Quantity);
 
- 
+    /**
+     * @brief Check if the item that player sell is valid according to their role
+     * 
+     * @param item code 
+     * @param player role 
+     * 
+     * @return true if valid 
+     * @return false if not valid (Non Mayor want to sell buildings)
+     */
+    bool isSalevalid (string code, string role);
+
+    
+    
 
 private:
-    /**
-     * @brief Set the initial Qty of every object, Where Product and Building init Qty = 0, else Qty = infinite 
-     * 
-     * @param _Qty 
-     */
-    void set_Qty(int _Qty);
+
+    /*************** GET Data from other .hpp *************************/
 
      /**
-     * @brief Show the needed Animal's vector data
+     * @brief Get Code from other .hpp for initial value
      * 
-     * \note only show Num. Name - Price
      * 
-     * @param ?
+     * @return string Code from other .hpp
+     * 
      */
-    void Show_Animal_Inventory();
+    string getCode_outer() const;
 
     /**
-     * @brief Show the needed Plant's vector data
+     * @brief Get Price from other .hpp for initial value
      * 
-     * \note only show Num. Name - Price
+     * @return int Price from other .hpp
      * 
-     * @param ?
      */
-    void Show_Plant_Inventory();
+    int getPrice_outer() const;
+
+    /*************** SET Data to cotr *************************/
 
     /**
-     * @brief Show the needed Product's vector data
+     * @brief Set the item_number from 1 to the last elmt 
      * 
-     * \note only show Num. Name - Price - Qty
+     * @param int current item_number
      * 
-     * @param ?
      */
-    void Show_Product_Inventory();
+    void set_ID(int _id);
 
     /**
-     * @brief Show the needed Building's vector data
+     * @brief Set the Code into the ctor ?
      * 
-     * \note only show Num. Name - Price - Qty
-     * 
-     * @param ?
+     * @param string code
      */
-    void Show_Building_Inventory();
+    void set_Code(string code);
+
+    /**
+     * @brief Set the Price into the ctor ?
+     * 
+     * @param int Price
+     */
+    void set_Price(int Price);
+
+    /**
+     * @brief Set the Qty into the ctor ?
+     * 
+     * \note for Animal and Plant, Qty = INFINITY
+     * \note for else, Qty = 0
+     * 
+     * @param int Qty
+     */
+    void set_Code(int Qty);
+
+
+
+};
+
 
       
 
@@ -219,7 +236,7 @@ if (Validate_buy(itemNumber, quantity) ){
     setWealth(gulden_update)
 
      //Ambil data dengan itemNumber itu 
-    T = get_Data(itemNumber).drop(ID).drop(Qty)
+    T = get_Data(itemNumber).drop(item_number).drop(Qty)
 
     output_update(gulden_update, quantity, T)
 
@@ -265,113 +282,60 @@ if n != 0 {
 
 
 
-
-
-
-
-
-
-
-/*
-
-Shop. 
-
-Di dalam shop ada inventory, yang berisi data semua item yg tersedia dalam game - Baca Config + Qty yang tersedia
-
-PR = How bisa ambil data semua item ? 
-
-Sementara: 
-Dalam inventory ada 4 vektor, yaitu vector<Animal>, vector<Plant>, vector<Product + Qty>, dan vector<Building + Qty>
-
-dan set Qty = 0
-
-
-
-
-
-
-*/
-    
-
-    /*
-    void Jual(Player p);
-    //update inventori dari shop. Cek role ! -> Jika role = Petani atau Peternak gk bisa jual bangunan. 
-    //Terima role 
-    //Buka Inventori + Cetak open_inventori()
-    //Minta user untuk select koordinat inventori yg mau dijual dengan memastikan role. Jika role = Petani atau Peternak gk bisa jual bangunan. 
-    //Validasi koordinat apakah ada atau gk. Kalau 
-
-    void Beli(player p);
-    //BELI
-    //Player: update status inventori dan gulden nya role. 
-    //Shop: Keluarin barang dari inventori. Cek role ! -> Jika role = Walikota gk bisa akses menu bangunan. 
-    // Terima data player. 
-    // Cek role, tampilkan data sesuai role
-    // Show_Shop()
-    // Print out berapa gulden dan slot kosong nya player
-    // Gulden anda : "Gulden"
-    // Slot kosong : "Slot"
-    // if Slot == 0 or Gulden == 0 -> Gak bisa beli krn slot/Gulden == 0
-    // else -> Minta input barang apa + kuantitas
-    // input kuantitas. Jika slot kosong >= kuantitas DAN kuantitas*harga barang <= total gulden -> Valid. If salah satu gk terpenuhi, ulang lagi (LOOP).
-    // if valid, update nSlot dan Gulden player. -> Print gulden
-    // input_inventori_player()
-
-    void Show_Shop(string role);
-    //Menampilkan inventori toko, nama barang - kuantitas
-    //Terima role player -> Tampilkan data sesuai role. 
-    // Format : 
-    // Nama Barang - Harga G - Qty
-
-    virtual void Show_Shop_Menu();
-    //Menapilkan menu
-    
-    */
-
-
-
-};
-
-
-/*
-//Child dari Shop, Tugas sama seperti shop tapi harga lebih rendah 30%(?) dan hanya ada tiap 4 turn
-class Black_Market : public Shop{
-
-    void Show_Shop_Menu() override;
-
-
-};
-
-*/
-
-
 class BlackMarket: public Shop{
+    protected:
+
+    public:
+        /**
+         * @brief ctor user defined
+         * 
+         * @param Black Market Qty
+         * 
+         */
+        BlackMarket(int Qty_BM);
+
+        
+        //**************** GETTER SETTER *****************//
+
+        /**
+         * @brief Get the Black Market Qty of item with Code = code
+         * 
+         * @param string code
+         * 
+         * @return int Qty
+         */
+        int GetQtyBM(string code);
+
+
+        //*************** Show Black Market Inventory *******************//
+
+        /**
+         * @brief Show limited BlackMarket Inventory Based on Role
+         * 
+         * @param string Role
+         * 
+         */
+        void Show_Shop_Inventory(string Role, int BMList) const;
+
+
+    private:
+        /**
+         * @brief Set a Random number for Black Market Qty of item with Code = code
+         * 
+         * \note QtyBM = min(Qty, rand(1,10))
+         * 
+         * @param code 
+         * @return int 
+         */
+        int SetQtyBM(string code);
+
+        /**
+         * @brief Get a list of random number
+         * 
+         */
+
 
 };
-
-
-/*
-
-Black Market
-
-Kelas Turunan dari Shop. 
-
-Inventory Black Market ambil dari Inventory
-
-Sementara: 
-Dalam inventory ada 4 vektor, yaitu vector<Animal+Qty_BM_1>, vector<Plant+Qty_BM_1>, vector<Product+Qty + Qty_BM>, dan vector<Building+Qty + Qty_BM>
-
-dan set Qty_BM_1 = rand(min_qty,max_qty) dan set Qty_BM = min(Qty, rand(min_qty, max_qty))
-
-Untuk methode semua inheritance dari Shop. 
-
-
-
-
-
-
-
-*/
 
 
 
