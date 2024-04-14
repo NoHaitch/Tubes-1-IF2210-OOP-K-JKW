aeb339708d8baec301f00884e0f3f0e2f1a764f0
fabianradenta <13522105@mahasiswa.itb.ac.id>
2024-04-15 04:54:06 +0700

    feat: add mayor

--- src/header/mayor.hpp
+++ src/header/mayor.hpp
@@ -2,12 +2,27 @@
 #define __MAYOR__
 
 #include "player.hpp"
+#include "building.hpp"
+#include "farmer.hpp"
+#include "cattleman.hpp"
+#include "product.hpp"
+#include "game.hpp"
+#include <vector>
+#include <iostream>
+using namespace std;
 
 class Mayor : public Player {
-    public:
-        Mayor(string _username);
-        Mayor(string _username, int _wealth, int _currWeight);
-        ~Mayor();
+protected :
+    vector<Building> buildingForSale;
+public:
+    Mayor(string _username);
+    Mayor(string _username, int _wealth, int _currWeight);
+    ~Mayor();
+    void pungutPajak();
+    void bangunBangunan();
+    void tambahPemain();
+    void buy() override;
+    void sell() override;
 };
 
 
--- src/mayor.cpp
+++ src/mayor.cpp
@@ -1,7 +1,47 @@
 #include "header/mayor.hpp"
 
-Mayor::Mayor(string _username) : Player(_username){}
-Mayor::Mayor(string _username, int _wealth, int _currWeight) 
-    : Player(_username, _wealth, _currWeight){}
+Mayor::Mayor(string _username) :
+    Player(_username),
+    buildingForSale(){}
+
+Mayor::Mayor(string _username, int _wealth, int _currWeight) :
+    Player(_username, _wealth, _currWeight),
+    buildingForSale(){}
 
 Mayor::~Mayor(){}
+
+void Mayor::pungutPajak(){
+    cout << "Cring cring cring..." << endl;
+    cout << "Pajak sudah dipungut!" << endl;
+    cout << endl << "Berikut adalah detil dari pemungutan pajak:" << endl;
+
+}
+
+void Mayor::tambahPemain(){
+    if (wealth<50){
+        cout << "Uang tidak cukup!" << endl;
+    }
+    else {
+        string playerType, plName;
+        cout << "Masukkan jenis pemain: ";
+        cin >> playerType;
+        if (!(playerType == "peternak" || playerType == "petani")){
+            throw FileFormatException("Jenis pemain salah!");
+        }
+        cout << "Masukkan nama pemain: ";
+        cin >> plName;
+
+        if (playerType == "peternak"){
+            Cattleman peternak(plName);
+            // pushback peternak ke vector cattleman di game
+        }
+        else {
+            Farmer petani(plName);
+            // pushback petani ke vector farmer di game
+        }
+    }
+}
+
+void Mayor::bangunBangunan(){
+    
+}
